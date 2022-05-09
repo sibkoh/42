d6dd53c75dd04ec741cd063c4bf28fecfec929af
Alejandro Munoz de la sierra <amunoz-d@c1r8s2.42malaga.com>
2022-05-09 13:23:13 +0200

    empezando bonus

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: amunoz-d <amunoz-d@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 10:50:59 by amunoz-d          #+#    #+#             */
-/*   Updated: 2022/05/09 12:19:01 by amunoz-d         ###   ########.fr       */
+/*   Updated: 2022/05/09 13:19:25 by amunoz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,13 @@ void		ft_putstr_fd(char *s, int fd);
 void		ft_putchar_fd(char c, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list		*ft_lstnew(void *content);
 
 #endif
